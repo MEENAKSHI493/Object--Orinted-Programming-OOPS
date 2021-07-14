f15b47b2b4bb794f683fc596c4ba2ca7c0c3df5d
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-14 23:02:04 +0530

    Decords

---
+++ Bridgelabz/InventoryManagementSevice.java
@@ -0,0 +1,5 @@
+package com.Bridgelabz;
+
+public interface InventoryManagementSevice {
+
+}
---
+++ Bridgelabz1/StockAccountManagement.java
@@ -0,0 +1,11 @@
+package com.Bridgelabz1;
+
+public interface StockAccountManagement {
+	
+    public  void addStocks(Stocks share);
+	public  void getStockDetails();
+	public  void calculateShareValue();
+	public  void removeStock(String name);
+	public  void sellStocks(int count, String symbol);
+	public  void buyStocks(int count, String symbol);
+}
